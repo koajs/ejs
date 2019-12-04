129021bc9bae23910434ebba7a61b82b8245f6ea
Anthony Howe <SirWumpus@users.noreply.github.com>
2019-12-03 22:42:34 -0500

    chore: Move definition of resolveInclude() override out of closure

--- index.js
+++ index.js
@@ -68,6 +68,14 @@ exports = module.exports = function (app, settings) {
     ? '.' + settings.viewExt.replace(/^\./, '')
     : '';
 
+  // override `ejs` node_module `resolveInclude` function
+  ejs.resolveInclude = function(name, filename, isDir) {
+    if (!path.extname(name)) {
+      name += settings.viewExt;
+    }
+    return parentResolveInclude(name, filename, isDir);
+  }
+
   /**
    * generate html with view name and options
    * @param {String} view
@@ -84,14 +92,6 @@ exports = module.exports = function (app, settings) {
     }
 
     const tpl = await fs.readFile(viewPath, 'utf8');
-
-    // override `ejs` node_module `resolveInclude` function
-    ejs.resolveInclude = function(name, filename, isDir) {
-      if (!path.extname(name)) {
-        name += settings.viewExt;
-      }
-      return parentResolveInclude(name, filename, isDir);
-    }
 
     const fn = ejs.compile(tpl, {
       filename: viewPath,
