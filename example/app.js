16f01faf7d6f1e972f02e1ff533572a329639b13
3imed-jaberi <imed_jebari@hotmail.fr>
2021-07-14 00:47:58 +0200

    update the example with ejs-v3 spec 🍾 ..

--- example/app.js
+++ example/app.js
@@ -4,48 +4,46 @@
  * MIT Licensed
  */
 
-'use strict';
+'use strict'
 
 /**
  * Module dependencies.
  */
 
-const Koa = require('koa');
-const render = require('..');
-const path = require('path');
+const Koa = require('koa')
+const render = require('..')
+const path = require('path')
 
-const app = new Koa();
+const app = new Koa()
 
 render(app, {
   root: path.join(__dirname, 'view'),
   layout: 'template',
   viewExt: 'html',
   cache: false,
-  debug: false,
-});
+  debug: false
+})
 
-app.use(function (ctx, next) {
-  ctx.state = ctx.state || {};
-  ctx.state.now = new Date();
-  ctx.state.ip = ctx.ip;
-  ctx.state.version = '2.0.0';
-  return next();
-});
-
-app.use(async function (ctx) {
-  const users = [{ name: 'Dead Horse' }, { name: 'Jack' }, { name: 'Tom' }];
-  await ctx.render('content', {
-    users
-  });
-});
+app
+  .use(function (ctx, next) {
+    ctx.state = ctx.state || {}
+    ctx.state.now = new Date()
+    ctx.state.ip = ctx.ip
+    ctx.state.version = '2.0.0'
+    return next()
+  })
+  .use(async function (ctx) {
+    const users = [{ name: 'Dead Horse' }, { name: 'Imed Jaberi' }, { name: 'Tom' }]
+    await ctx.render('content', { users })
+  })
 
 if (process.env.NODE_ENV === 'test') {
-  module.exports = app.callback();
+  module.exports = app.callback()
 } else {
-  app.listen(7001);
-  console.log('open http://localhost:7001');
+  app.listen(7001)
+  console.log('open http://localhost:7001')
 }
 
 app.on('error', function (err) {
-  console.log(err.stack);
-});
+  console.log(err.stack)
+})
